9415fecf659f459618e2f4067aee54c1ece18730
M.Shiva Nandheswara Reddy <mnandu0011@gmail.com>
2026-06-17 08:11:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function showMessage() {
-    alert("Welcome to my website!");
+function sub() {
+    let no = "";
+
+    for (let i = 0; i < 9; i++) {
+        no += Math.floor(Math.random() * 10);
+    }
+
+    document.getElementById("m").innerHTML =
+        `<h3 style="color:green">
+            Application Complete!<br>
+            Application Number: ${no}
+        </h3>`;
+}
+
+function previewImage(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+        const img = document.getElementById("p");
+        img.src = URL.createObjectURL(file);
+        img.style.display = "block";
+    }
 }
